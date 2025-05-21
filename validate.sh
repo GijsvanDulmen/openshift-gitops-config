635cc0bdc51dbac818bb7e6606d65b31524e3c67
Gijs van Dulmen <gijs@vandulmen.net>
2025-05-21 21:55:42 +0200

    Remove old structure

--- validate.sh
+++ validate.sh
@@ -3,5 +3,6 @@
 # jv: https://github.com/santhosh-tekuri/jsonschema
 
 # TODO: make this obviously not explicitely checking only those two tenants
-./jv ./config/onprem-dev/onprem-dev-1/tenants/team-backend/values.yaml
-./jv ./config/onprem-dev/onprem-dev-1/tenants/team-frontend/values.yaml
+# TODO: make this work with overlapping values
+# ./jv ./config/onprem-dev/onprem-dev-1/tenants/team-backend/values.yaml
+# ./jv ./config/onprem-dev/onprem-dev-1/tenants/team-frontend/values.yaml
